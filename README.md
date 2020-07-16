468d98dc1ca846c22e77deab2a64e4dd8d207ffd
nicknamedelta <evaldo.felipe2@gmail.com>
2020-07-15 22:14:09 -0300

    1step - PIL, shades, conversion

---
+++ README.md
@@ -0,0 +1,16 @@
+# px2txt
+
+(●⌒∇⌒●)b- "Sure, i will convert this image to Ascii!"
+
+## What was used ?
+
+Installation: [Pillow](https://pillow.readthedocs.io/en/stable/installation.html)
+
+## :runner: To run this project ?
+
+On project's root folder run `python3 main.py`
+
+## Notes:
+
+- Get file path on Linux: run `realpath <file>.<extension>` in file folder
+  - [How to get full path of a file ?](https://stackoverflow.com/a/45690418)
---
+++ main.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# pylint: disable=missing-docstring
+import math
+from PIL import Image
+
+def img_convert(path, ref, width, height):
+    # define shades and range
+    shades = [ '.', ':', ';', 'i', 'r', 's', 'X', 'A', '2', '5', '3', 'h', 'M', 'H', 'G', 'S', '#', '9', 'B', '&', '@']
+    s_range = 255 / len(shades)
+
+    # convert pixels in shades
+    pixels = []
+    for y in range(0, height):
+        line = []
+        for x in range(0, width):
+            point = ref.getpixel((x, y))
+            pixel = math.ceil(point[0] + point[1] + point[2] / 3)
+
+            shade = ''
+            if (pixel >= s_range * 4):
+                shade = shades[0]
+            elif (pixel >= s_range * 3):
+                shade = shades[1]
+            elif (pixel >= s_range * 2):
+                shade = shades[2]
+            elif (pixel >= s_range * 1):
+                shade = shades[3]
+            else:
+                shade = shades[4]
+            
+            line.append(shade)
+        pixels.append(''.join(line) + '\n')
+    write_file(''.join(pixels))
+
+    # remove the comment below to see the result in the terminal
+    # print(''.join(pixels))
+
+def write_file(text):
+    # open new file in append mode
+    f = open("cnv_img.txt", "w")
+
+    # write and close file
+    f.write(text)
+    f.close()
+
+def main():
+    # get image real path of user
+    img_path = input('(≧∀≦)ゞ - Tell to me what\'s the path to image: ')
+    img = Image.open(img_path)
+
+    # resize img to 100px x 100px
+    img_ref = img.resize((100, 100))
+
+    # resize image and override the file
+    img_convert(img_path, img_ref, img_ref.width, img_ref.height)
+    print('(∩^o^)⊃━☆゜.* - "It\'s over, the result are in cnv_img.txt"')
+
+main()
